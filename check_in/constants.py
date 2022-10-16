4062bfc33aeacb99105638d9f24ec8c640ef380c
Mirabror Mirzokhidov <mirzokhido@fim.uni-passau.de>
2022-10-16 09:56:39 +0000

    refactor: API

---
+++ check_in/constants.py
@@ -0,0 +1,5 @@
+EMPLOYEE_LOG_TYPE_IN = "IN"
+EMPLOYEE_LOG_TYPE_OUT = "OUT"
+
+EMPLOYEE_LOG_COMP_TYPE = "Alternating entries as IN and OUT during the same shift"
+EMPLOYEE_WORK_HRS_COMP_TYPE = "Every Valid Check-in and Check-out"
