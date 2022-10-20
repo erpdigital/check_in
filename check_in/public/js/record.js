2237b467fd2700df55ebdd6e0067d632a43bf1c5
AlimerdanRahimov <AlimerdanRahimov@gmail.com>
2022-10-20 19:23:50 +0000

    Revert "record js"
    This reverts commit 4be76a63d0197690b4cd7ebfda0a78317677b0ea.

--- check_in/public/js/record.js
+++ check_in/public/js/record.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
     let time = date.toLocaleTimeString();
+
+
     // This arrangement can be altered based on how we want the date's format to appear.
     let currentDate = `${day}-${month}-${year}`;
-    document.getElementById("current_date").innerText = "Today is:" + currentDate;
-    document.getElementById("current_time").innerText = "Time is:" + time;
+    document.getElementById("now").innerText = "Today is:" + currentDate +" " + time;
 
 
     onscan.attachTo(document,
@@ -36,15 +37,12 @@ $(document).ready(function(){
 
     function populate(message, log_type) {
       var result = JSON.parse(message);
-      document.getElementById('employee_name').innerText = result.employee_name;
-      document.getElementById('employee_designation').innerText = result.designation;
-      document.getElementById("employee_image").src = 'http://site.localhost:8000' + result.image;
+      document.getElementById('name').innerText = result.employee_name;
+      document.getElementById("image").src = 'http://site.localhost:8000' + result.image;
       const date = new Date().toLocaleTimeString();
-     (log_type == "IN") ? document.getElementById("checkin").innerText = "Check In Time "+ date : document.getElementById("checkin").innerText = "Check Out Time "+ date;
-      (log_type == "OUT")? document.getElementById("total_time").innerText= " Worked total hours:" + result.totalHours: document.getElementById("total_time").innerText="";
-      (log_type == "OUT")? document.getElementById("greeting").innerText= "Good bye!" :  document.getElementById("greeting").innerText= "Hello";
-      $("#welcome_block").hide();
-      $("#checkin_block").show();
+      (log_type == "IN") ? document.getElementById("check").innerText = "Check In Time "+ date : document.getElementById("check").innerText = "Check Out Time "+ date;
+      (log_type == "OUT")? document.getElementById("totalHours").innerText= " Worked total hours:" + result.totalHours: "";
+      $("#employee").show();
     }
     
     // TODO function for date
