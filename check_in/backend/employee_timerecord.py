c4c492b2323c7e35c5da2d43a7ea1373107dd630
AlimerdanRahimov <AlimerdanRahimov@gmail.com>
2022-10-23 15:08:41 +0000

    save

--- check_in/backend/employee_timerecord.py
+++ check_in/backend/employee_timerecord.py
@@ -7,44 +7,43 @@ from frappe.utils.data import nowdate
 from check_in.backend.constants import *
 from check_in.backend.model import *
 from datetime import timedelta
-    
+
+
 class EmployeeTimeRecord:
     def __init__(self, attendance_id, type) -> None:
         self.attendance_id = attendance_id
         self.type = type
-       
 
-    def make(self):        
-        return self.make_checkin() if(self.type == EMPLOYEE_LOG_TYPE_IN) else self.make_checkout()   
-          
+    def make(self):
+        return self.make_checkin() if (self.type == EMPLOYEE_LOG_TYPE_IN) else self.make_checkout()
 
     def make_checkin(self):
         self.get_employee()
         self.set_check_in()
         self.get_working_hours()
         self.set_attendance()
-        result=self.employee._asdict()
+        result = self.employee._asdict()
         result['totalHours'] = self.working_hours
-        return json.dumps(result)  
-
+        return json.dumps(result)
 
     def make_checkout(self):
         self.get_employee()
         self.set_check_out()
         self.get_working_hours()
         self.set_attendance()
-        result=self.employee._asdict()
+        result = self.employee._asdict()
         result['totalHours'] = self.working_hours
-        return json.dumps(result) 
+        result['lastCheckin'] = self.last_checkin
+        return json.dumps(result)
 
-    
     def check_status(self):
         self.get_employee()
         return self.get_status()
 
     def get_last_checkin(self):
-        self.last_checkin = frappe.get_last_doc('Employee Checkin', filters={"device_id": self.attendance_id})
-    
+        self.last_checkin = vars(frappe.get_last_doc('Employee Checkin', filters={
+                                           "device_id": self.attendance_id}))['time'].isoformat()
+ 
     def get_status(self):
         is_attendance_exist = frappe.db.exists(
             'Attendance', {"employee": self.employee.name, "attendance_date": nowdate()})
@@ -55,14 +54,14 @@ class EmployeeTimeRecord:
         else:
             return EMPLOYEE_LOG_TYPE_OUT
 
-
+  
     def get_employee(self):
         self.employee = frappe.db.get_value("Employee", {'attendance_device_id': self.attendance_id},
-                                                        ["name", "employee_name", "company", "designation", "default_shift", "image"])
-        employee1, name, company,designation,shift, image = frappe.db.get_value("Employee", {'attendance_device_id': self.attendance_id},
-                                                        ["name", "employee_name", "company", "designation", "default_shift", "image"])
-        self.employee=ModelEmployee(employee1,name,company,designation,shift,image)
-        
+                                            ["name", "employee_name", "company", "designation", "default_shift", "image"])
+        employee1, name, company, designation, shift, image = frappe.db.get_value("Employee", {'attendance_device_id': self.attendance_id},
+                                                                                  ["name", "employee_name", "company", "designation", "default_shift", "image"])
+        self.employee = ModelEmployee(
+            employee1, name, company, designation, shift, image)
 
     def set_check_in(self):
         self.doc = frappe.new_doc("Employee Checkin")
@@ -72,9 +71,11 @@ class EmployeeTimeRecord:
         self.doc.device_id = self.attendance_id
         self.doc.log_type = EMPLOYEE_LOG_TYPE_IN
         self.doc.insert()
+
+        doc1 = frappe.get_doc('Employee', self.employee.name)
+        frappe.client.set_value('Employee',doc1.name,'online','Online')
         frappe.db.commit()
-        
-        
+
     def set_check_out(self):
         self.doc = frappe.new_doc("Employee Checkin")
         self.doc.employee = self.employee.name
@@ -83,30 +84,31 @@ class EmployeeTimeRecord:
         self.doc.device_id = self.attendance_id
         self.doc.log_type = EMPLOYEE_LOG_TYPE_OUT
         self.doc.insert()
+        doc1 = frappe.get_doc('Employee', self.employee.name)
+        frappe.client.set_value('Employee',doc1.name,'online','Offline')
         frappe.db.commit()
-
+        frappe.db.commit()
 
     def get_working_hours(self):
         doc = frappe.db.get_list("Employee Checkin", filters={'creation': ['>=', nowdate()], 'employee': self.employee.name},
-                             fields=['time', 'log_type'], order_by='time desc')
+                                 fields=['time', 'log_type'], order_by='time desc')
 
-        self.working_hours = self.calculate_working_hours(doc, EMPLOYEE_WORK_HRS_COMP_TYPE)
-       
-        
-       
+        self.working_hours = self.calculate_working_hours(
+            doc, EMPLOYEE_WORK_HRS_COMP_TYPE)
+
     def set_attendance(self):
         is_exist = frappe.db.exists("Attendance", {
-                                "employee": self.employee.name, "attendance_date": nowdate(), "docstatus": ("!=", "2")})
+            "employee": self.employee.name, "attendance_date": nowdate(), "docstatus": ("!=", "2")})
         if not is_exist:
-            doc_dict ={
-            "doctype": "Attendance",
-            "employee": self.employee.name,
-            "attendance_date": nowdate(),
-            "status": "Present",
-            "company": self.employee.company,
-            "shift": self.employee.shift,
-            "in_time": now_datetime(),
-                        }
+            doc_dict = {
+                "doctype": "Attendance",
+                "employee": self.employee.name,
+                "attendance_date": nowdate(),
+                "status": "Present",
+                "company": self.employee.company,
+                "shift": self.employee.shift,
+                "in_time": now_datetime(),
+            }
 
             attendance = frappe.get_doc(doc_dict).insert()
             attendance.submit()
@@ -116,26 +118,25 @@ class EmployeeTimeRecord:
         else:
             if self.type == EMPLOYEE_LOG_TYPE_OUT:
                 attendance_name = frappe.db.get_value(
-                "Attendance", {'employee': self.employee.name, "attendance_date": nowdate()}, "name")
-                
+                    "Attendance", {'employee': self.employee.name, "attendance_date": nowdate()}, "name")
+
                 # Linking checkout doc to Attendance
                 self.doc.attendance = attendance_name
                 self.doc.save()
 
                 frappe.db.set_value('Attendance', attendance_name, {
-                                'out_time':  now_datetime(), 'working_hours': self.working_hours})
+                    'out_time':  now_datetime(), 'working_hours': self.working_hours})
                 frappe.db.commit()
             else:
                 attendance_name = frappe.db.get_value(
-                "Attendance", {'employee': self.employee.name, "attendance_date": nowdate()}, "name")
+                    "Attendance", {'employee': self.employee.name, "attendance_date": nowdate()}, "name")
                 # Linking checkin doc to Attendance
                 self.doc.attendance = attendance_name
                 self.doc.save()
 
                 frappe.db.set_value('Attendance', attendance_name, {
-                                'working_hours': self.working_hours})
+                    'working_hours': self.working_hours})
                 frappe.db.commit()
-
 
     def calculate_working_hours(self, logs, working_hours_calc_type):
         """
@@ -146,10 +147,10 @@ class EmployeeTimeRecord:
         if working_hours_calc_type == EMPLOYEE_WORK_HRS_COMP_TYPE:
             logs = logs[:]
             while len(logs) >= 2:
-                total_hours += self.time_diff_in_hours(logs[0].time, logs[1].time)
+                total_hours += self.time_diff_in_hours(
+                    logs[0].time, logs[1].time)
                 del logs[:2]
         return total_hours
-
 
     def time_diff_in_hours(self, start, end):
         time = -1 * round((end - start).total_seconds() / 3600, 1)
