0c84a780e58d2c266dc9033f3ac3a6802fe9aad4
AliDevel <alimerdanrahimov@gmail.com>
2022-12-30 13:18:07 +0000

    new

--- check_in/public/js/timerecord.js
+++ check_in/public/js/timerecord.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   
     function check_in() {
         frappe.call('check_in.api.check_in', {     })
-          .then(r => {      app.innerHTML = 'Check Out';
+          .then(r => {     
+          app = document.getElementById('check')  
+          app.innerHTML = 'Check Out';
           app.onclick = function(){check_out();}
           app.classList= "btn btn-info"
 
@@ -11,7 +13,9 @@ $(document).ready(function(){
       
       function check_out() {
         frappe.call('check_in.api.check_out', {})
-          .then(r => {       app.innerHTML = 'Check In';
+          .then(r => {     
+          app = document.getElementById('check')  
+          app.innerHTML = 'Check In';
           app.classList= "btn btn-info"
           app.onclick =function(){check_in();}
      
@@ -23,13 +27,13 @@ $(document).ready(function(){
 	.then(r => {  		  
        
         var app = document.createElement('button');
+        app.id = 'checkin'
+        app.classList= "btn btn-info"
         if (r.message == "OUT"){
             app.innerHTML = 'Check In';
-            app.classList= "btn btn-info"
             app.onclick = function(){check_in();}
         } else {
             app.innerHTML = 'Check Out';
-            app.classList= "btn btn-info"
             app.onclick = function(){check_out();}
         }
         
