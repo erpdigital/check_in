8249133fee1ba33793c8702123212535eeff797e
AliDevel <alimerdanrahimov@gmail.com>
2023-01-01 16:55:16 +0000

    Bugs fixed

--- check_in/public/js/timerecord.js
+++ check_in/public/js/timerecord.js
@@ -1,25 +1,30 @@
 $(document).ready(function(){
-  
+
+
+
+
     function check_in() {
+        frappe.show_progress('Check In..', 70, 100, 'Please wait', hide_on_completion = true);
         frappe.call('check_in.api.check_in', {     })
           .then(r => {     
-            alert('hello');
-          app = document.getElementById('check')  
+          app = document.getElementById('checkin')  
           app.innerHTML = 'Check Out';
           app.onclick = function(){check_out();}
           app.classList= "btn btn-info"
+          frappe.show_progress('Check In..', 100, 100, 'Please wait', hide_on_completion = true);
 
         })
       }
       
       function check_out() {
+        frappe.show_progress('Check Out..', 70, 100, 'Please wait', hide_on_completion = true);
         frappe.call('check_in.api.check_out', {})
           .then(r => {     
-          app = document.getElementById('check')  
+          app = document.getElementById('checkin')  
           app.innerHTML = 'Check In';
           app.classList= "btn btn-info"
           app.onclick =function(){check_in();}
-     
+          frappe.show_progress('Check Out..', 100, 100, 'Please wait', hide_on_completion = true);
         });
     }
 
@@ -37,7 +42,7 @@ $(document).ready(function(){
             app.innerHTML = 'Check Out';
             app.onclick = function(){check_out();}
         }
-        
+         
 	 $('header.navbar > .container > .navbar-collapse > ul.navbar-nav').prepend(app); 
     
     
